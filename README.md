580bbe989f6429aa5830daf6c54dd71701e35086
Moon Kang <mkang987@gmail.com>
2021-11-20 15:58:29 -0500

    Edited JS to add high score, updated read me

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ This is a timed quiz about coding in general to help us prepare for any future i
 
 Once start is pressed, user will get asked questions one at a time, up to 10 questions.
 
+![Picture of high score screen](./assets/img/img2.png)
+
 if wrong answer is chosen, timer is reduced by 8 seconds.
 
 Game is over once all questions are answered or time has run out.
@@ -10,3 +12,7 @@ Game is over once all questions are answered or time has run out.
 Total point is correct answers + time left.
 
 At the end of the screen user will get asked for their name to show on the highscore leader board.
+
+![Picture of high score screen](./assets/img/img1.png)
+
+https://mkang987.github.io/Code-Quiz/
--- assets/java/java.js
+++ assets/java/java.js
@@ -8,8 +8,9 @@ var questionDisplay = document.querySelector(".question");
 var questionBox = document.querySelector("#sectionbox");
 var scoreCount = document.querySelector("#currentScore");
 var totalScore = document.querySelector("#totalScore");
-var endScore = document.querySelector("#highscore");
-
+var endScore = document.querySelector("#scoreBoard");
+var viewScore = document.querySelector("#highscore-button");
+var clearScore = document.querySelector("#clear-button");
 
 var timer = 60;
 var timerCount = 60;
@@ -18,76 +19,81 @@ var questionNumber = 0;
 var rightAnswer;
 var chosenAnswer;
 
+const NO_OF_HIGH_SCORES = 5;
+const HIGH_SCORES = 'highScores';
+
+
+//List of Questions and Correct answer
 var questions = [ 
     {
-        question: "What is my name?",
-        choice1: "Steve",
-        choice2: "John",
-        choice3: "Alex",
-        choice4: "Raccoon",
-        correctAnswer: "3"
-    },
-
-    {
-        question: "Correct Answer is 1",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
+        question: "What does HTML Stand for?",
+        choice1: "HyperText Markup Language",
+        choice2: "High Transfer Module Lists",
+        choice3: "Hidden Text Mockup Launcher",
+        choice4: "Held Terminal Minimized Loader",
         correctAnswer: "1"
     },
 
     {
-        question: "super long text that in the end its going to tell me that the answer is choice number 2. hopefully this will fit in the box or something",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
-        correctAnswer: "2"
-    },
-
-    {
-        question: "Question 4 answer 4",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
+        question: "Who invented HTML?",
+        choice1: "Bill Gates",
+        choice2: "Edward Jenner",
+        choice3: "John Pemberton",
+        choice4: "Tim Berners-Lee",
         correctAnswer: "4"
     },
 
     {
-        question: "Question 5 answer 2",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
+        question: "What is DOM?",
+        choice1: "Data Onsite Migration",
+        choice2: "Document Object Model",
+        choice3: "Digital Opposing Membrane",
+        choice4: "Dynamic Operational Module",
         correctAnswer: "2"
     },
 
     {
-        question: "Question 6 answer 2",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
+        question: "A ____ is used for creating an element in HTML.",
+        choice1: "README",
+        choice2: "Style Sheet",
+        choice3: "Title Bar",
+        choice4: "Tag",
+        correctAnswer: "4"
+    },
+
+    {
+        question: "Which of the following is a function?",
+        choice1: "rollTheDice();",
+        choice2: "var x = 5;",
+        choice3: "<title>Coding is fun!</title>",
+        choice4: ".inline{display: inline}",
+        correctAnswer: "1"
+    },
+
+    {
+        question: "A(n) ______ extends a tag, changing tag behavior or providing metadata.",
+        choice1: "Addon",
+        choice2: "Attribute",
+        choice3: "Identifier",
+        choice4: "Morpher",
         correctAnswer: "2"
     },
 
     {
-        question: "Question 7 answer 2",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
+        question: "What is the difference between <a> and <link> tag?",
+        choice1: "<a> is used for setting arguments while <link> will link 2 elements",
+        choice2: "<a> links to another webpage or certain part of webpage, <link> defines link between a doc and external source.",
+        choice3: "<a> will activate whatever is inside it, <link> will link whatever objects are inside it.",
+        choice4: "No differences",
         correctAnswer: "2"
     },
 
     {
-        question: "Question 8 answer 2",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
+        question: "When was jQuery released?",
+        choice1: "September 8th, 2008",
+        choice2: "August 26, 2006",
+        choice3: "July 19th, 2004",
+        choice4: "May 9th, 2010",
         correctAnswer: "2"
     },
 
@@ -137,13 +143,14 @@ function timeLeft() {
 }
 
 
-//game starer
+//game starer function
 function startgame() {
     timer = 60;
     timerCount = 60;
     SCORE = 0;
     questionNumber = 0;
     timeLeft();
+    endScore.style.display = "none";
     questionBox.style.display = "block";
     startbutton.style.visibility = "hidden";
     nextQuestion();
@@ -178,26 +185,64 @@ function checkAnswer() {
     }
 }
 
-//Game rules.
-//Show question with 4 answer, if wrong answer remove 8 seconds.
-//Total score (Correct + timeleft)
 
 //End Screen
 
 function endScreen() {
     SCORE = SCORE + timerCount;
     totalScore.textContent = SCORE;
-    endScore.style.display = "block";
+    startbutton.style.visibility = "visible";
+    checkHighScore(SCORE)
 }
 
 //Refresh Score screen
-
 function checkScore() {
     scoreCount.textContent = SCORE;
 }
 
+
+//HighScore function
+function checkHighScore(SCORE) {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+
+    if(SCORE > lowestScore) {
+        saveHighScore(SCORE, highScores);
+        showHighScores();
+    }
+}
+
+//View highscores
+function showHighScores() {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const highScoreList = document.getElementById(HIGH_SCORES);
+  
+    endScore.style.display = "block";
+    highScoreList.innerHTML = highScores
+        .map((SCORE) => `<li>${SCORE.SCORE} - ${SCORE.name}`)
+        .join('');
+}
+
+//Saving HighScore
+function saveHighScore(SCORE, highScores) {
+    var name = prompt('Enter your name:');
+    var newScore = { SCORE , name};
+    highScores.push(newScore);
+    highScores.sort((a,b) => b.SCORE - a.SCORE);
+    highScores.splice(NO_OF_HIGH_SCORES);
+    localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+}
+
+//Clear high Scores
+function clearHighScores() {
+    localStorage.clear();
+    showHighScores();
+}
+
 //Event listener for start button
 startbutton.addEventListener("click", startgame);
+viewScore.addEventListener("click",showHighScores)
+clearScore.addEventListener("click",clearHighScores)
 
 //Event listener for answers. Change event when finishing.
 answer1.addEventListener("click", function(event) {
